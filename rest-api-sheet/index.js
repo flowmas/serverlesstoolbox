10d9e3cf8686d00709096ec6c0907ae0bba8c527
Sam Wolf <sam1008@icloud.com>
2020-03-24 02:10:12 -0700

    Update index.js

--- rest-api-sheet/index.js
+++ rest-api-sheet/index.js
@@ -51,9 +51,9 @@ app.get('/', async (req, res) => {
 });
 
 async function getHours() {
-  const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1qObCPH0-8zxjvG5KcbmT2iMk40KffE1LPhj0UhBFDh8/values/Sheet1?key=AIzaSyBZ95WJEcD7pL8QD83EyAsWSWTxoqQo2Cc');
+  const response = await $.get('https://sheets.googleapis.com/v4/spreadsheets/1qObCPH0-8zxjvG5KcbmT2iMk40KffE1LPhj0UhBFDh8/values/Sheet1?key=AIzaSyBZ95WJEcD7pL8QD83EyAsWSWTxoqQo2Cc');
   const myJson = await response.json();
-  return await myJson;
+  return myJson;
 //   return {
 //     id: '1',
 //     name: 'Snohomish Store',
