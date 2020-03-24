474857e9618fbbaebef36f24c574683a775fbee7
Sam Wolf <sam1008@icloud.com>
2020-03-24 15:24:30 -0700

    Update index.js

--- rest-api-sheet/index.js
+++ rest-api-sheet/index.js
@@ -1,48 +1,28 @@
 const express = require('express');
-//const https = require('https')
 const app = express();
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Costco Hours listening on port', port);
-});
+app.listen(8080);
 
 app.get('/', async (req, res) => {
+  const hours = getHours();
   res.setHeader('Content-Type', 'application/json');
   let retVal;
-  var hours = { 'id': '1','name': 'Snohomish Store', 'warehouseLocation': 'Snohomish, WA', 'ZIP': '98290', 'date': '3/24/2020'};
   if (hours) {
-    retVal = {status: 'success', data: {hours: d}};
-    console.log(retVal);
+    retVal = {status: 'success', data: {hours: hours}};
+    console.log(retVal)
   } else {
     res.status(404);
     retVal = {status: 'fail', data: {title: `hours not found`}};
   }
-  res.json(retVal);
+  res.json(hours);
+});
+
+function getHours() {
+  return {
+    id: '1',
+    name: 'Snohomish Store',
+    warehouseLocation: 'Snohomish, WA',
+    ZIP: '98290',
+    date: '3/24/2020',
+  }
 }
-        
-// async function getHours() {
-//   var data
-//   const options = {
-//     hostname: 'sheets.googleapis.com',
-//     port: 443,
-//     path: '/v4/spreadsheets/1qObCPH0-8zxjvG5KcbmT2iMk40KffE1LPhj0UhBFDh8/values/Sheet1?key=AIzaSyBZ95WJEcD7pL8QD83EyAsWSWTxoqQo2Cc',
-//     method: 'GET'
-//   };
-
-// const req = https.request(options, res => {
-//   console.log(`statusCode: ${res.statusCode}`);
-
-//   res.on('data', d => {
-//       process.stdout.write(d);
-//       return d;
-//      });
-//    })
-
-//   req.on('error', error => {
-//     console.error(error);
-//   })
-
-//   req.end();
-  
-// }
