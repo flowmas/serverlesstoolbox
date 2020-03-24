47131e32e9d9f6dd691fac21ca2e76ac4ca37123
Sam Wolf <sam1008@icloud.com>
2020-03-24 15:06:13 -0700

    Update index.js

--- rest-api-sheet/index.js
+++ rest-api-sheet/index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const https = require('https')
 const app = express();
 
-
-
 const port = process.env.PORT || 8080;
 app.listen(port);
 
-//   return {
-//     'id': '1',
-//     'name': 'Snohomish Store',
-//     'warehouseLocation': 'Snohomish, WA',
-//     'ZIP': '98290',
-//     'date': '3/24/2020'
-//   };
-
-
+app.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  let retVal;
+  var hours = await getHours();
+  if (hours) {
+    retVal = {status: 'success', data: {hours: d}};
+    console.log(retVal);
+  } else {
+    res.status(404);
+    retVal = {status: 'fail', data: {title: `hours not found`}};
+  }
+  res.json(retVal);
+}
+        
 function getHours() {
   var data
   const options = {
@@ -30,29 +33,20 @@ const req = https.request(options, res => {
 
   res.on('data', d => {
       process.stdout.write(d);
-
-      app.get('/', async (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        let retVal;
-        if (hours) {
-          retVal = {status: 'success', data: {hours: d}};
-          console.log(retVal)
-        } else {
-          res.status(404);
-          retVal = {status: 'fail', data: {title: `hours not found`}};
-        }
-        console.log(res.json(retVal));
-
-      });
-    })
-  })
+      return d;
+     });
+   })
 
   req.on('error', error => {
     console.error(error);
   })
 
   req.end();
+  return {
+    'id': '1',
+    'name': 'Snohomish Store',
+    'warehouseLocation': 'Snohomish, WA',
+    'ZIP': '98290',
+    'date': '3/24/2020'
+  };
 }
-
-console.log("Returned Data = " + JSON.stringify(getHours()));
-console.log("Finished getHours()");
